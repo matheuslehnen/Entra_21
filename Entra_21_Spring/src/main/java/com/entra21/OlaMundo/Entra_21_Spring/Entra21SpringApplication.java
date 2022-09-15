3880bd634c5799cad1ec40e3a103fb3b70324740
Matheus Lehnen <matheuslehnen@hotmail.com>
2022-09-14 21:42:23 -0300

    Primeiras aulas de Spring

---
+++ Entra_21_Spring/src/main/java/com/entra21/OlaMundo/Entra_21_Spring/Entra21SpringApplication.java
@@ -0,0 +1,13 @@
+package com.entra21.OlaMundo.Entra_21_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entra21SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entra21SpringApplication.class, args);
+	}
+
+}
---
+++ Entra_21_Spring/src/test/java/com/entra21/OlaMundo/Entra_21_Spring/Entra21SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.OlaMundo.Entra_21_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entra21SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
